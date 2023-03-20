55c1587a1aaf191edc5b40ba3e92bba10b896fd2
Zhy <zhyryllposadas@gmail.com>
2023-03-20 09:35:49 +0800

    Project 7 - Messenger Website

---
+++ Project-7-Messenger-Website/script.js
@@ -0,0 +1,130 @@
+const milly = () => {
+    const milly = document.getElementById("name");
+    milly.innerHTML = "Milly Bobby Brown";
+    const milly1 = document.getElementById("name-again");
+    milly1.innerHTML = "Milly Bobby Brown";
+    const pic = document.getElementById("pic");
+    pic.src = "img/Brown.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/Brown.jpg";
+    picture.style.width = "40px";
+
+};
+const avengers = () => {
+    const avengers = document.getElementById("name");
+    avengers.innerHTML = "Avengers";
+    const avengers1 = document.getElementById("name-again");
+    avengers1.innerHTML = "Avengers";
+    const pic = document.getElementById("pic");
+    pic.src = "img/marvel.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/marvel.jpg";
+    picture.style.width = "40px";
+
+};
+const theboys = () => {
+    const theboys = document.getElementById("name");
+    theboys.innerHTML = "The Boys";
+    const theboys1 = document.getElementById("name-again");
+    theboys1.innerHTML = "The Boys";
+    const pic = document.getElementById("pic");
+    pic.src = "img/theboys.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/theboys.jpg";
+    picture.style.width = "40px";
+
+};
+const billie = () => {
+    const billie = document.getElementById("name");
+    billie.innerHTML = "Billie Eilish";
+    const billie1 = document.getElementById("name-again");
+    billie1.innerHTML = "Billie Eilish";
+    const pic = document.getElementById("pic");
+    pic.src = "img/billy.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/billy.jpg";
+    picture.style.width = "40px";
+
+};
+const maive = () => {
+    const maive = document.getElementById("name");
+    maive.innerHTML = "Maive Wiley";
+    const maive1 = document.getElementById("name-again");
+    maive1.innerHTML = "Maive Wiley";
+    const pic = document.getElementById("pic");
+    pic.src = "img/maive.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/maive.jpg";
+    picture.style.width = "40px";
+
+};
+const lucifer = () => {
+    const lucifer = document.getElementById("name");
+    lucifer.innerHTML = "Lucifer Morningstar";
+    const lucifer1 = document.getElementById("name-again");
+    lucifer1.innerHTML = "Lucifer Morningstar";
+    const pic = document.getElementById("pic");
+    pic.src = "img/lucifer.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/lucifer.jpg";
+    picture.style.width = "40px";
+
+};
+const rick = () => {
+    const rick = document.getElementById("name");
+    rick.innerHTML = "Rick Grimes";
+    const rick1 = document.getElementById("name-again");
+    rick1.innerHTML = "Rick Grimes";
+    const pic = document.getElementById("pic");
+    pic.src = "img/rick.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/rick.jpg";
+    picture.style.width = "40px";
+
+};
+const zhyryll = () => {
+    const zhyryll = document.getElementById("name");
+    zhyryll.innerHTML = "Zhyryll Ryj Posadas";
+    const zhyryll1 = document.getElementById("name-again");
+    zhyryll1.innerHTML = "Zhyryll Ryj Posadas";
+    const pic = document.getElementById("pic");
+    pic.src = "img/zhy.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/zhy.jpg";
+    picture.style.width = "40px";
+
+};
+const daenarys = () => {
+    const daenarys = document.getElementById("name");
+    daenarys.innerHTML = "Daenarys Targaryen";
+    const daenarys1 = document.getElementById("name-again");
+    daenarys1.innerHTML = "Daenarys Targaryen";
+    const pic = document.getElementById("pic");
+    pic.src = "img/daenarys.png";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/daenarys.png";
+    picture.style.width = "40px";
+
+};
+const heisenberg = () => {
+    const heisenberg = document.getElementById("name");
+    heisenberg.innerHTML = "Heisenberg";
+    const heisenberg1 = document.getElementById("name-again");
+    heisenberg1.innerHTML = "Heisenberg";
+    const pic = document.getElementById("pic");
+    pic.src = "img/walter.jpg";
+    pic.style.height = "70px";
+    const picture = document.getElementById("picture");
+    picture.src = "img/walter.jpg";
+    picture.style.width = "40px";
+
+};
